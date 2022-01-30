4453bbe8d99ac7cd9c72bd0f91be4f0314b7d408
ElizPN <pankevych.liza@gmail.com>
2022-01-30 22:06:44 +0200

    finding sum using recursion

---
+++ sumTo.js
@@ -0,0 +1,18 @@
+function sumTo(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(sumTo(100));
+
+function sumNum(n) {
+  if (n < 1) {
+    return n;
+  } else {
+    return n + sumNum(n - 1);
+  }
+}
+console.log(sumNum(100));
