d91cff6b8c00ff797411ce4f6d50a3771497d287
ElizPN <pankevych.liza@gmail.com>
2022-01-30 22:11:33 +0200

    creating new array and add numbers to it using recursion

---
+++ recusion2.js
@@ -0,0 +1,20 @@
+function countup(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countup(n - 1); // Recursive call - when a function calls itself with progressively smaller values of n until it reaches 1.
+    countArray.push(n);
+    return countArray;
+  }
+}
+console.log(countup(5));
+
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    let newArr = countdown(n - 1);
+    newArr.unshift(n);
+    return newArr;
+  }
+}
